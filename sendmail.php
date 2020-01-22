5502a121f2d3e9d842357cfcfb6183d685ecac17
bselpin <bselpin@naver.com>
2020-01-22 14:03:56 +0900

    no message

--- sendmail.php
+++ sendmail.php
@@ -17,22 +17,19 @@ $frommail = $_POST['frommail'];
 $subject = $_POST['subject'];
 $subject2 = "$name \r $hp \r $frommail \r $subject";
 
-if(!$name)message("이름을 입력하세요");
-if(!$corporation)message("기업/기관명을 적으세요");
-if(!$hp)message("연락처를 적으세요");
-if(!$frommail)message("메일을 적으세요");
-if(!$subject)message("내용을 적으세요");
+if(!$name)message("이름을 입력해 주세요");
+if(!$corporation)message("기업/기관명을 입력해 주세요");
+if(!$hp)message("연락처를 입력해 주세요");
+if(!$frommail)message("메일을 입력해 주세요");
+if(!$subject)message("내용을 입력해 주세요");
  
 $tomail = "purpleseed@purpleseed.co.kr" ;
  
 /* mail()함수에서 쓰게될 변수 */
  
-
- 
 /* 줄의 끝에 붙어 있는 슬래쉬를 제거하는 함수 */
  
-$subject=stripslashes($subject);
- 
+$subject=stripslashes($subject); 
  
 /* 메일을 보내주는 함수 */
  
