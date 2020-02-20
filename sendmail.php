2b6fbb1aceff76cb222994f584963ae64155dd9f
bselpin <bselpin@naver.com>
2020-02-20 22:24:27 +0900

    no message

--- sendmail.php
+++ sendmail.php
@@ -1,42 +1,16 @@
-<?
-function message($message)
-{
-echo "
-<script>
-window.alert(\"$message\");
-history.go(-1);
-</script>
-";
+<?php
 
+if(isset($_POST['name'], $_POST['corporation'],$_POST['hp'],$_POST['frommail'],$_POST['subject'], $_POST['text'])) {
+    $name = $_POST['name'];
+    $corporation = $_POST['corporation'];
+    $hp = $_POST['hp'];
+    $frommail = $_POST['frommail'];
+    $subject = $_POST['subject'];   
+    $subject=stripslashes($subject);
+    $text = $_POST['text'];
+    $tomail = "purpleseed@purpleseed.co.kr" ;
+    $subject2 = "$name \r $hp \r $corporation \r $frommail \r $text";
+    mail($tomail, $subject, $subject2);
 }
- 
-$name = $_POST['name'];
-$corporation = $_POST['corporation'];
-$hp = $_POST['hp'];
-$frommail = $_POST['frommail'];
-$subject = $_POST['subject'];
-$subject2 = "$name \r $hp \r $frommail \r $subject";
 
-if(!$name)message("이름을 입력해 주세요");
-if(!$corporation)message("기업/기관명을 입력해 주세요");
-if(!$hp)message("연락처를 입력해 주세요");
-if(!$frommail)message("메일을 입력해 주세요");
-if(!$subject)message("내용을 입력해 주세요");
- 
-$tomail = "purpleseed@purpleseed.co.kr" ;
- 
-/* mail()함수에서 쓰게될 변수 */
- 
-/* 줄의 끝에 붙어 있는 슬래쉬를 제거하는 함수 */
- 
-$subject=stripslashes($subject); 
- 
-/* 메일을 보내주는 함수 */
- 
-mail($tomail, $subject, $subject2);
- 
-echo '<script language="javascript">';
-echo 'alert("메일이 발송되었습니다.")';
-echo '</script>';
- 
 ?>
